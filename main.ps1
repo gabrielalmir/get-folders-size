8f2c26216522b1364a97006b962bce1cd45351cd
momentoalmir <momentoalmir@gmail.com>
2023-04-25 09:56:33 -0300

    feature: generic parser

--- main.ps1
+++ main.ps1
@@ -32,12 +32,40 @@ function ParseJsonFile {
 
     # Loop através das propriedades do objeto JSON
     foreach ($item in $json.PSObject.Properties) {
-        $hash[$item.Name] = $item.Value
+        # $hash[$item.Name] = $item.Value
+        if ($null -eq $hash[$item.Name]) {
+            $hash[$item.Name] = @()
+        }
+        $hash[$item.Name] += $item.Value
     }
 
     # Retornar objeto Hashable
     return $hash
 }
+
+function ParseFile {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$FilePath
+    )
+
+    # Obter a extensão do arquivo
+    $extension = [System.IO.Path]::GetExtension($FilePath)
+
+    # Verificar a extensão do arquivo
+    switch ($extension.ToLower()) {
+        # JSON
+        ".json" {
+            return ParseJsonFile $FilePath
+        }
+        # Extensão não suportada
+        default {
+            Write-Host "Extensao nao suportada." -ForegroundColor Red
+            return
+        }
+    }
+}
+
 
 function GetFoldersSize {
     [CmdletBinding()]
@@ -157,7 +185,7 @@ function ExecuteGetFoldersSize {
 
 Measure-ScriptBlock {
     # Ler arquivo JSON e converter para objeto Hashable
-    $locations = (ParseJsonFile -FilePath "./locations.json")
+    $locations = (ParseFile -FilePath "./locations.json")
     # or
     # $locations = @{
     #     "server1" = @(
